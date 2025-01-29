249d351581d7bf249deaab10ff33430297f598a3
tomaszszz <tomasz.rozko97@gmail.com>
2025-01-29 10:02:54 +0100

    initial commit

---
+++ src/main/java/com/prediction/demo/integration/FinancialDataAcquirer.java
@@ -0,0 +1,22 @@
+package com.prediction.demo.integration;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+
+@Service
+public class FinancialDataAcquirer {
+    private static final String API_KEY = "cucurf9r01qri16ors1gcucurf9r01qri16ors20";
+
+    private final WebClient webClient = WebClient.create();
+
+    public Mono<String> getDataFromApi() {
+        String apiUrl = "https://finnhub.io/api/v1/quote?symbol=AAPL&token=" + API_KEY;
+        return webClient.get()
+                .uri(apiUrl)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+}
